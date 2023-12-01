2db205c904f2144c433d0148c8a7ab2db6114481
jeongsol <jeongsol@outlook.com>
2023-12-01 19:38:16 +0900

    save test

--- src/test/java/com/example/todolist/TodolistApplicationTests.java
+++ src/test/java/com/example/todolist/TodolistApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.todolist;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TodolistApplicationTests {
 
+	@Autowired
+	private TaskRepository taskRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Task task1 = new Task();
+		task1.setItem("첫번째할일");
+		task1.setCreatedDate(LocalDateTime.now());
+
+		this.taskRepository.save(task1);
+		
+
 	}
 
 }
